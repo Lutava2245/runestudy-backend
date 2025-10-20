f01752257e975909e37a78064e41d594a99397ab
Luan T. Felix <lutavares.bros@gmail.com>
2025-10-20 14:01:59 -0300

    :sparkles: implementa controlador de autenticação e adiciona novos endpoints para usuários - adiciona endpoint de login para gerar um JWT - adiciona endpoint para registro de usuários - define proteção de endpoints por roles de usuário

--- src/main/java/com/fatec/runestudy/controller/UserController.java
+++ src/main/java/com/fatec/runestudy/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fatec.runestudy.domain.dto.UserCreateDTO;
 import com.fatec.runestudy.domain.dto.UserResponseDTO;
 import com.fatec.runestudy.service.UserService;
 
@@ -11,7 +12,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("api/users")
@@ -21,19 +26,30 @@ public class UserController {
     private UserService userService;
 
     @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<List<UserResponseDTO>> getAllUsers() {
         List<UserResponseDTO> users = userService.getAll();
         return users.isEmpty()
-            ? ResponseEntity.ok(users)
-            : ResponseEntity.notFound().build();
+            ? ResponseEntity.notFound().build()
+            : ResponseEntity.ok(users);
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
     public ResponseEntity<UserResponseDTO> getUser(@RequestParam Long id) {
         UserResponseDTO user = userService.getById(id);
         return user != null
             ? ResponseEntity.ok(user)
             : ResponseEntity.notFound().build();
+    }
+    
+    
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDTO> registerUser(@RequestBody UserCreateDTO requestDTO) {
+        UserResponseDTO user = userService.createUser(requestDTO);
+        return user != null
+            ? ResponseEntity.ok(user)
+            : ResponseEntity.badRequest().build();
     }
     
 }
