2b3c95ee9cda8e7d45612ff66c476c50b7ba0e0d
Luan T. Felix <lutavares.bros@gmail.com>
2025-11-21 15:30:37 -0300

    :sparkles: adiciona DTOs para a entidade Reward

---
+++ src/main/java/com/fatec/runestudy/domain/dto/response/RewardResponse.java
@@ -0,0 +1,20 @@
+package com.fatec.runestudy.domain.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class RewardResponse {
+
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+	private int price;
+
+	private String status;
+
+}
